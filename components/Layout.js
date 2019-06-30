3d4f86e147e3c80ebac4ca45e1332399eb81aa33
ABM97 <viragesud97@gmail.com>
2019-06-30 14:57:55 +0200

    Added header and layout for front-end pages

---
+++ components/Layout.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+import Head from "next/head";
+import Header from "./Header";
+
+export default props => {
+    return (
+        <div>
+            <Header />
+            <Container>
+                <Head>
+                    <link
+                        rel="stylesheet"
+                        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Exo+2:400,700i&display=swap"
+                    />
+                </Head>
+                <style jsx global>{`
+          * {
+            font-family: "Exo 2", sans-serif;
+          }
+        `}</style>
+                <div>{props.children}</div>
+            </Container>
+        </div>
+    );
+};
