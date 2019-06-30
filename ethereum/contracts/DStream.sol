da2f99098b3f4188e1a0b1e4a0f4074673d586a5
Awamry <omarelawamry@gmail.com>
2019-06-30 14:44:59 +0200

    Modified signature logic in like,dislike and view functions

--- ethereum/contracts/DStream.sol
+++ ethereum/contracts/DStream.sol
@@ -1,12 +1,14 @@
+// solium-disable linebreak-style
 pragma solidity >=0.4.19 <0.7.0;
 
 contract DStream {
+  
     
     //checking if the video's hash is already uploaded or not.
     mapping(string=>bool)videos;
     
     //array to store addresses of the uploaded videos on DStream's platform
-    address [] public uploadedVideos;
+    address[] public uploadedVideos;
     
     function uploadVideo(string memory ipfsHash, string memory metaDataHash, string memory category)public{
         require(!videos[ipfsHash]);
@@ -23,9 +25,11 @@ contract DStream {
     function getUploadedVideos() public view returns (address[] memory){
         return uploadedVideos;
     }
-    
-}
 
+    function isUploaded(string memory hash) public view returns (bool){
+        return videos[hash];
+    }
+}
 
 // Video's a smart contract 
 contract Video {
@@ -69,27 +73,65 @@ contract Video {
         Function used to verify user signture by comparing his address with address
         extracted from signature
     */
-    function verify(bytes32 _hash, uint8 _v, bytes32 _r, bytes32 _s, address _address)
+    function verify(bytes32 _hash,  bytes memory sig, address _address)
          public pure returns (bool) {
-
-            //An ethereum related prefix that should be hashed with the original messaage
-            bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-
-             //hash the prefix and the message together
-            bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, _hash));
-
             /*
                  If the prefixedHash and the signtaure (v,r,s) returned the user's address, then we can verify
                      that this user has signed the message with his private key
              */
-            return ecrecover(prefixedHash, _v, _r, _s) == _address;
+            
+            return recoverSigner(_hash, sig) == _address;
+    }
+    
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+                
+
+    }
+    
+    function splitSignature(bytes memory sig)
+    public
+    pure
+    returns (uint8, bytes32, bytes32)
+    {
+        require(sig.length == 65);
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        return (v, r, s);
     }
 
-    function likeVideo(uint8 _v, bytes32 _r, bytes32 _s, address _user) public{
-      
+    function recoverSigner(bytes32 message, bytes memory sig)
+        public
+        pure
+        returns (address)
+    {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = splitSignature(sig);
+        
+        return ecrecover(prefixed(message), v, r, s);
+    }
+
+    function likeVideo(bytes memory _sig, address _user) public{
+        
         //We make sure that the user intended to like the video
         //by checking if he signed the message ("like"+ videohash) with his private key
-        require(verify(keccak256(abi.encodePacked("like",IPFSHash)),_v,_r,_s,_user));
+        bytes32 likestamp = keccak256(abi.encodePacked("like",IPFSHash));
+         require(verify(likestamp,_sig,_user));
       
         //if the user has liked this video before and clicked the like button again
          if(userToVideoInteractionStatus[_user]==Status.LIKING){
@@ -135,12 +177,13 @@ contract Video {
         if user has already disliked the video => it changes user state from like to neutral
      */
 
-    function dislikeVideo(uint8 _v, bytes32 _r, bytes32 _s, address _user) public {
+    function dislikeVideo(bytes memory _sig, address _user) public {
         /* 
             We make sure that the user intended to like the video
-            by checking if he signed the message ("like"+ videohash) with his private key
+            by checking if he signed the message ("dislike"+ videohash) with his private key
         */
-        require(verify(keccak256(abi.encodePacked("dislike",IPFSHash)),_v,_r,_s,_user));
+        bytes32 dislikestamp = keccak256(abi.encodePacked("dislike",IPFSHash));
+         require(verify(dislikestamp,_sig,_user));
 
         // Getting current status of user, Default = Netural
         Status userStatus = userToVideoInteractionStatus[_user];
@@ -158,13 +201,12 @@ contract Video {
       }
   }
 
-  function viewVideo(uint8 _v, bytes32 _r, bytes32 _s, address _address) public{
-      require(!userToVideoViewStatus[_address]);
-      require(verify(keccak256(abi.encodePacked("view",IPFSHash)),_v,_r,_s,_address));
-      userToVideoViewStatus[_address]=true;
+  function viewVideo(bytes memory _sig, address _user) public{
+      require(!userToVideoViewStatus[_user]);
+      bytes32 viewstamp = keccak256(abi.encodePacked("view",IPFSHash));
+      require(verify(viewstamp,_sig,_user));
+      userToVideoViewStatus[_user]=true;
       numViews++;
   }
 
-
-    
 }
